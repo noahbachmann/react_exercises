abfc5220943013257c6c5636a6eb078d178d1fc1
Noah <trainpaths@gmail.com>
2023-11-15 17:04:33 +0100

    created todo app only created the components so far

---
+++ 02-props-state-component-architecture/react-components/my-react-app-todo/src/Todo.js
@@ -0,0 +1,11 @@
+import './App.js';
+
+function Todo(){
+    return(
+        <div className="todo">
+            
+        </div>
+    )
+}
+
+export default Todo;
---
+++ 02-props-state-component-architecture/react-components/my-react-app-todo/src/TodoList.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Todo from './Todo';
+
+function TodoList(){
+    return(
+        <div className="list">
+
+        </div>
+    )
+}
+
+export default TodoList;
