475e85dd125b2ba0e200c516d47714926c0a92ae
Noah <trainpaths@gmail.com>
2023-11-16 13:38:39 +0100

    did the todo app

--- 02-props-state-component-architecture/react-components/my-react-app-todo/src/Todo.js
+++ 02-props-state-component-architecture/react-components/my-react-app-todo/src/Todo.js
@@ -7,7 +7,7 @@ function Todo({ task, checked, onDelete, onChange }){
             <form className='todo-form'>
                 <input type="checkbox" checked={checked} id="todoItem" name="todoItem" onChange={onChange}/>
                 <p id="todoText" className='todo-info'>{task}</p>
-                <button onClick={onDelete} value="Delete"/>
+                <button className="delete-button" onClick={onDelete}>Delete</button>
             </form>
         </div>
     )
